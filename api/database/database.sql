5f8c5363f08e91df70545dd299162dcca56ccbf8
Roark98 <roberto.izurieta@gmail.com>
2022-12-06 23:55:56 +0100

    Hito 3 - Update

---
+++ api/database/database.sql
@@ -0,0 +1,22 @@
+CREATE TABLE LUGAR(
+    id SERIAL PRIMARY KEY,
+	image TEXT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    cost double precision NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    region VARCHAR(255) NOT NULL,
+    visit_count INTEGER NOT NULL default 0,
+	review_counter INTEGER NOT NULL default 0,
+	review_value DOUBLE PRECISION NOT NULL default 0
+)
+
+CREATE TABLE USERS(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    favorites_array INTEGER[],
+)
--- api/src/config.js
+++ api/src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   db: {
     user: process.env.DB_USER || "postgres",
     password: process.env.DB_PASSWORD || "1379",
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "host.docker.internal",
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_DATABASE || "ecuatourism_test",
   },
