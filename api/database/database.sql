c85586d92d1d47f7aaec12e92570021ce9252375
Roark98 <roberto.izurieta@gmail.com>
2022-12-21 22:32:18 +0100

    Compose modify

--- api/database/database.sql
+++ api/database/database.sql
@@ -10,13 +10,12 @@ CREATE TABLE LUGAR(
     visit_count INTEGER NOT NULL default 0,
 	review_counter INTEGER NOT NULL default 0,
 	review_value DOUBLE PRECISION NOT NULL default 0
-)
-
+);
 CREATE TABLE USERS(
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(255) NOT NULL,
-    favorites_array INTEGER[],
+    favorites_array INTEGER[]
 )
